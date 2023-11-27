237f4b3ad098eaf1b98296ba7dac2843971056e7
AristaaSingh <146640656+AristaaSingh@users.noreply.github.com>
2023-11-27 18:12:46 +0000

    27nov1812

--- experiment/exp.c
+++ experiment/exp.c
@@ -6,7 +6,7 @@ int main()
     int a;
     int b;
     printf("First number: ");
-    scanf("%d", &b);
+    scanf("%d", &a);
     printf("Second number: ");
     scanf("%d", &b);
     printf("%d\n", sumIntegers(a,b));
--- w7/code.c
+++ w7/code.c
@@ -32,8 +32,8 @@ int main()
 
         printf("A: View all your blood iron levels\n");                       // BRONZE
         printf("B: View your average blood iron level\n");                    // BRONZE
-        printf("C: View your lowest blood iron level\n");                     // SILVER
-        printf("D: View your highest blood iron level\n");                    // SILVER
+        printf("C: View your highest blood iron level\n");                     // SILVER
+        printf("D: View your lowest blood iron level\n");                    // SILVER
         printf("E: View the blood iron levels for a specific month\n");       // SILVER/GOLD
         printf("F: See some additional statistics about your iron levels\n"); // GOLD - see readme.md
         printf("G: Generate a graph of your iron levels\n");                  // GOLD/PLATINUM - see readme.md
@@ -63,7 +63,7 @@ int main()
             }
             for (int i = 0; i < counter; i++)
             {
-                printf("%s - Blood iron: %.1f\n", daily_readings[i].date, daily_readings[i].bloodIron);
+                printf("%s - Blood iron: %.2f\n", daily_readings[i].date, daily_readings[i].bloodIron);
             }
             fclose(input);
             break;
@@ -86,17 +86,48 @@ int main()
 
         case 'C':
         case 'c':
-            return 0;
+            counter = 0;
+            float max = 0;
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                if (daily_readings[counter].bloodIron > max){
+                    max = daily_readings[counter].bloodIron;
+                }
+                counter++;
+            }
+            printf("Your highest blood iron level is: %.2f\n", max);
             break;
 
         case 'D':
         case 'd':
-            return 0;
+            counter = 0;
+            float min = 100000;
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                if (daily_readings[counter].bloodIron < min){
+                    min = daily_readings[counter].bloodIron;
+                }
+                counter++;
+            }
+            printf("Your lowest blood iron level is: %.2f\n", min);
             break;
 
         case 'E':
         case 'e':
-            return 0;
+            counter = 0;
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                counter++;
+            }
             break;
 
         case 'F':
