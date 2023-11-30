e9e74ade844178faa1e3503976c68dabde4c750b
AristaaSingh <146640656+AristaaSingh@users.noreply.github.com>
2023-11-30 00:18:23 +0000

    task 2 finished, yay!

--- coursework/StepCounter.c
+++ coursework/StepCounter.c
@@ -37,6 +37,7 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 }
 
+// function to read the file line by line and read the records into the struct of FITNESS_DATA type
 int file_to_struct(FILE *inputFile, FITNESS_DATA *dataArray)
 {
     int counter = 0;
@@ -59,6 +60,16 @@ int file_to_struct(FILE *inputFile, FITNESS_DATA *dataArray)
 // Complete the main function
 int main() {
 
+    //defining variables needed throughout the program
+    int i;
+    int min = 100000;
+    int max = 0;
+    float mean_stepcount;
+    int line_number;
+    int period_start = 0;
+    int final_period_start = 0;
+    int period_length = 0;
+    int final_period_length = 0;
     char choice;
     char my_filename[100];
     int num_records;
@@ -66,6 +77,7 @@ int main() {
     FITNESS_DATA my_records[500];
 
     while(1){
+        // printing menu for user interface
         printf("Menu options:\n");
         printf("A: Specify the filename to be imported\n");
         printf("B: Display the total number of records in the file\n");
@@ -84,13 +96,14 @@ int main() {
                 printf("Input filename: ");
                 scanf("%s", my_filename);
                 file_pointer = fopen(my_filename, "r");
+                // to print error message if file does not exist
                 if (file_pointer == NULL) {
-                    printf("Error: Could not find or open the file.\n");
+                    printf("Error: Could not find or open the file.\n"); 
                     exit(1);
                 }
                 char line[buffer_size];
                 num_records = file_to_struct(file_pointer, my_records);
-                int i;
+                // to print error message if any of the value in the file is missing (unexpected format)
                 for (i = 0; i < num_records; i++){
                     if (my_records[i].date == NULL){
                         printf("Error: Could not find or open the file.\n");
@@ -115,22 +128,63 @@ int main() {
 
             case 'c':
             case 'C':
-                //int i;
-                //int min = 100000;
-                //int line_number;
-                //for (i = 0; i < num_records; i++){
-                //    if (my_records[i].steps < min){
-                //       min = my_records[i].steps;
-                //        line_number = i;
-                //    }
-                //printf("Fewest steps")
-                //}
-                return 0;
+                for (i = 0; i < num_records; i++){
+                    if (my_records[i].steps < min){
+                        min = my_records[i].steps;
+                        line_number = i;
+                    }
+                }
+                printf("Fewest steps: %s %s\n", my_records[line_number].date, my_records[line_number].time);
                 break;
-            
+
+            case 'd':
+            case 'D':
+                for (i = 0; i < num_records; i++){
+                    if (my_records[i].steps > max){
+                        max = my_records[i].steps;
+                        line_number = i;
+                    }
+                }
+                printf("Largest steps: %s %s\n", my_records[line_number].date, my_records[line_number].time);
+                break;
+
+            case 'e':
+            case 'E':
+                for (i = 0; i < num_records; i++){
+                    mean_stepcount += my_records[i].steps;
+                }
+                mean_stepcount = mean_stepcount / num_records;
+                printf("Mean step count: %.0f\n", mean_stepcount);
+                break;
+
+            case 'f':
+            case 'F':
+                for (i = 0; i < num_records; i++){
+                    if (my_records[i].steps >= 500) {
+                        period_length++; // period length keeps increasing if steps are continuously above 500
+                    } 
+                    else {
+                        // if the period length is the longest yet then set it as the final period length
+                        if (period_length > final_period_length) {
+                            final_period_length = period_length;
+                            final_period_start = period_start;
+                        }
+                        period_length = 0;
+                        period_start = i + 1; // defining the starting index for the period in the struct
+                    }
+                }
+                printf("Longest period start: %s %s\n", my_records[final_period_start].date, my_records[final_period_start].time);
+                printf("Longest period end: %s %s\n", my_records[final_period_start + final_period_length - 1].date, my_records[final_period_start + final_period_length - 1].time);
+                break;
+
             case 'q':
             case 'Q':
                 exit(0);
+                break;
+
+            // if the user types any other option
+            default:
+                printf("Invalid choice\n");
                 break;
         }
     }
