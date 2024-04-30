fc35598e0d78f4af456a51714f40d6a3878b12f7
AristaaSingh <146640656+AristaaSingh@users.noreply.github.com>
2024-04-30 18:41:49 +0000

    this code can read a space

--- experiment/filetoarray.c
+++ experiment/filetoarray.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    FILE *file = fopen("15x9.txt", "r");
+    FILE *file = fopen("myfile.txt", "r");
     if (file == NULL) {
         printf("Error: Could not open file.\n");
         return 1;
     }
 
+    for (int i = 0; i < 2; i++){
     int ch = fgetc(file);
     if (ch != EOF) {
         printf("Character read: %c\n", (char)ch);
     } else {
         printf("End of file reached.\n");
     }
+    }
+    
 
     fclose(file);
     return 0;
